7e436705d2fb0af1cd4ab324f204cd4d01d2a587
lama fall <falllama660@gmail.com>
2025-07-09 17:09:25 +0000

    first commit

---
+++ gestion_rendez_vous/Migrations/202503232356448_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿namespace gestion_rendez_vous.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        code = c.String(maxLength: 10, storeType: "nvarchar"),
+                        libelle = c.String(maxLength: 30, storeType: "nvarchar"),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.personnes", "IdRole", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.personnes", "IdRole");
+            DropTable("dbo.Roles");
+        }
+    }
+}
---
+++ gestion_rendez_vous/Migrations/202503240004002_AjoutModificationsModel.cs
@@ -0,0 +1,20 @@
+﻿namespace gestion_rendez_vous.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AjoutModificationsModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.personnes", "IdRole");
+            AddForeignKey("dbo.personnes", "IdRole", "dbo.Roles", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.personnes", "IdRole", "dbo.Roles");
+            DropIndex("dbo.personnes", new[] { "IdRole" });
+        }
+    }
+}
